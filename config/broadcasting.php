4a5c8ca25312c5df30c383a2163e900f83d847dc
Mohammad Alif Yasir bin Soleh <yasirsoleh@gmail.com>
2021-12-21 03:34:34 +0800

    config pusher

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -36,12 +36,9 @@ return [
             'secret' => env('PUSHER_APP_SECRET', 'app-secret'),
             'app_id' => env('PUSHER_APP_ID', 'app-id'),
             'options' => [
-                'host' => env('PUSHER_HOST', '127.0.0.1'),
-                'port' => env('PUSHER_PORT', 6001),
-                'scheme' => env('PUSHER_SCHEME', 'http'),
-                'encrypted' => true,
-                'useTLS' => env('PUSHER_SCHEME') === 'https',
-            ],
+                'cluster' => 'ap1',
+                'useTLS' => true
+              ],
         ],
 
         'ably' => [
